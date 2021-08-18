cc5d5d70ca9afd774fb42e82ceea0c981b89d847
Ephraim Phil <effi188@gmail.com>
2021-08-18 12:47:51 +0300

    fixed errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,33 @@
+/* profile block variables */ 
 let profileEditButton = document.querySelector('.button_action_edit'); 
 let profileName = document.querySelector('.profile__name'); 
 let profileOccupation = document.querySelector('.profile__occupation'); 
 let profileEditExitButton = document.querySelector('.button_action_close-edit') 
- 
-let popupEdit = document.querySelector('.popup_window_edit'); 
+/* popup edit box variables */
+let popupEdit = document.querySelector('.popup'); 
 let popupForm = document.querySelector('.popup__form'); 
 let popUpName = document.querySelector('.popup__input_type_name'); 
 let popUpOccupation = document.querySelector('.popup__input_type_occupation'); 
  
+/* this function opens the edit popup */ 
 function openEditPopUp() { 
-  popupEdit.setAttribute('class', 'popup popup_window_edit popup_opened'); 
+  popupEdit.setAttribute('class', 'popup popup_window_edit popup_opened');
+  /* once the form opened the input name and the about me are filled with the profile info */ 
   popUpName.value = profileName.textContent; 
   popUpOccupation.value = profileOccupation.textContent; 
 } 
- 
+
+/* this function closes the edit box popup */ 
 function closeEditPopUp() { 
   popupEdit.setAttribute('class', 'popup popup_window_edit'); 
 } 
- 
+
+/* form submit handler function */
 function profileEditFormSubmit(evt) { 
   evt.preventDefault(); 
- 
+  
+  /* once the user click save or enter the new text input of input name and input about me 
+  are inserted to the user profile info on the page*/
   profileName.textContent = popUpName.value; 
   profileOccupation.textContent = popUpOccupation.value; 
   closeEditPopUp(); 
